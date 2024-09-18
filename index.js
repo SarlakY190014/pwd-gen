a8800b947af704981691573dbfbe2be259f0bad2
SarlakY190014 <yunusemresarlak04@gmail.com>
2024-09-18 09:30:45 +0200

    Added password generator

---
+++ index.js
@@ -0,0 +1,13 @@
+const generatePassword = (length, specialCharactersAllowed) => {
+    const length = length;
+    const charset = specialCharactersAllowed 
+        ? "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+" 
+        : "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    return password;
+}
+module.exports = generatePassword;
